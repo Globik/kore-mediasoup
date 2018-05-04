217c710b7c5e3dc1156a7d3ff7c8a3e58ce7853f
Globik <gru5@yandex.ru>
2018-05-04 05:11:02 -0400

    in

--- training/hash/ghash3/d.c
+++ training/hash/ghash3/d.c
@@ -11,18 +11,23 @@
 //#include <linux/list.h> mmmm
 #include <ee.h>
 ee_t*ee=NULL;
-const char*event_hello="hello";
+//const
+	char*event_hello="hello";
 //struct atomic_notifier_head *l;
 //struct notifier_block*hn;
 //struct foo*ld;
 //atomic_notifier_chain_regiser(l,hn);
 struct  media;
 typedef void(*on_close)(struct media*,char*);
+typedef int(*proto_create_room)(struct media*,char*);
+typedef void(*proto_on_new_room)(struct media*,char*);
 typedef void(*suka)(void*);
 typedef void(*emitter)(suka, char*);
 struct media{
 	int a;
 	on_close cb_close;
+	proto_create_room create_room;
+	proto_on_new_room on_new_room;
 };
 struct nota{
 emitter emit;
@@ -30,9 +35,12 @@ emitter emit;
 void on_emitter(suka,char*);
 void bsuka(void*);
 void on_media_close(struct  media*,char*);
-struct media*media_new(on_close,struct nota);
+struct media*media_new(on_close,proto_on_new_room,struct nota);
 void notify(struct media*,char*);
-void create_room(struct media*);
+
+int create_room(struct media*,char*);
+void on_new_Room(struct media*,char*);
+//void create_room(struct media*);
 struct nota emi;
 
 void on_added_new_listener(void*arg){
@@ -54,6 +62,10 @@ int main(){
 	//ee_destroy(ee);
 	//ee=NULL;
 	if(ee==NULL)exit(1);
+	/*int i;
+	for(i=0;i<7;i++){
+	ee_on(ee,3,
+	}*/
 	ee_on(ee,EE_NEW_LISTENER,on_added_new_listener);
 	printf("count listen: %d\n",ee_listener_count(ee,event_hello));
 	//ee_on(ee,event_hello,on_hello);
@@ -67,33 +79,50 @@ int main(){
 	printf("eee : %p\n",ee);
 	emi.emit=on_emitter;
 	
-struct media*m=media_new(on_media_close, emi);
+struct media*m=media_new(on_media_close,on_new_Room, emi);
 if(m==NULL)exit(1);
 printf("zifr_1: %d\n",m->a);
+int j=m->create_room(m,"create_room");
+	/*
 notify(m,"room_created");
 printf("zifr_2: %d\n",m->a);
 notify(m,"room_closed");
 printf("zifr_3: %d\n",m->a);
 //atomic_notifier_chain_regiser(l,hn);
+*/
 free(m);
 ee_remove_listener(ee,event_hello,on_hello);
 ee_destroy(ee);
 return 0;
 }
-struct media*media_new(on_close cb,struct nota emi){
-struct media*m=(struct media*)calloc(1,sizeof*m);
+
+int create_room(struct media*m,char*s){
+if(m==NULL)printf("is null\n");
+printf("creating a room. : %s\n",s);
+notify(m,"room_created - success");
+return 0;
+}
+void on_new_Room(struct media*m,char*s){printf("on new room. : %s\n",s);}
+
+
+struct media * media_new(on_close cb, proto_on_new_room on_new_room_cb, struct nota emi){
+struct media * m=(struct media*)calloc(1,sizeof*m);
 if(m==NULL)return NULL;
 m->a=1;
 m->cb_close=cb;//this.on("event",(data)=>{}) ???
 //emi.emit("emitter runned.");
-ee_on(ee,event_hello,on_hello);
+//ee_on(ee,event_hello,on_hello);
+m->create_room=create_room;
+m->on_new_room=on_new_room_cb;
 return m;
 }
 // on_close proto
 void on_media_close(struct media*m,char*s){
 printf("on media close. event: %s\n",s);
 m->a=2;
-ee_emit(ee,event_hello,"fucker!!!!!!!");
+	//room created success notify client
+m->on_new_room(m,"ROOM CREATED!");
+//ee_emit(ee,event_hello,"fucker!!!!!!!");
 //emi.emit(bsuka,"closed");
 //ee_emit(ee,event_hello,"fucker!");
 }
